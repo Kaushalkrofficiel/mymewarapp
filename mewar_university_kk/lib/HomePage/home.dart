f6705c958ed6a261638653f75f10b0da5c60c457
Kaushalkrofficiel <157592605+Kaushalkrofficiel@users.noreply.github.com>
2024-10-25 16:05:06 +0530

    Update home.dart

--- mewar_university_kk/lib/HomePage/home.dart
+++ mewar_university_kk/lib/HomePage/home.dart
@@ -120,7 +120,7 @@ class _HomeState extends State<Home>
                               color: Colors.grey.withOpacity(0.5),
                               spreadRadius: 5,
                               blurRadius: 7,
-                              offset: Offset(0, 3), // changes position of shadow
+                              offset: Offset(0, 3), // changes position of shadow kkkkkkkkkk
                             ),
                         ]
                       
